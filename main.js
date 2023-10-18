3559bd9a1d1e75e4648db8c85cb3fdbedfd1a469
youi <145500357+asd34gbhfg@users.noreply.github.com>
2023-10-18 00:13:24 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ import { RedirectController } from './redirect-controller.js';
 
 // Inicializar la aplicación
 document.addEventListener('DOMContentLoaded', () => {
-    const redirectController = new RedirectController('https://www.facebook.com', 768);
+    const redirectController = new RedirectController('https://www.google.com', 768);
     redirectController.init();
 });
